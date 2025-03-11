8a9a3e9a9034d370df5e648775469907b2ea8dc8
Corey Ford <corey@coreyford.name>
2025-03-11 12:20:37 +0100

    Same graticule only, much more tractable

--- distinct_participant_streaks.py
+++ distinct_participant_streaks.py
@@ -3,8 +3,8 @@ import sys
 from datetime import date, timedelta
 
 # Incomplete streaks currently under consideration. Each is
-# (date, {
-#  frozenset(participants): [(expedition, participant), ...],
+# (date, graticule, {
+#  frozenset(participants): [participant, ...],
 #  ...
 # })
 # Later we can add an optional graticule to the tuple.
@@ -26,52 +26,50 @@ with open('alldata.js') as js:
   json_str = js.read()
   # Skip trailing comma
   json_str = json_str[json_str.find('['):json_str.rfind(']')-2] + ']'
-  print(json_str[-20:])
   for expedition in json.loads(json_str):
     day_str = expedition[0][0:10]
     graticule = expedition[0][11:]
     participants = expedition[3]
     success = expedition[4]
-    if success:
+    if success and participants:
       expeditions.setdefault(day_str, {})[graticule] = participants
 
 print("Loaded", len(expeditions), "expeditions", file=sys.stderr)
 
-day = date.today()#: date(2010, 5, 17)
-while day >= date(2008, 5, 1):
+day = date(2008, 5, 17)
+while day <= date.today(): #>= date(2008, 5, 1):
   day_str = day.isoformat()
   expeditions_day = expeditions.get(day_str, {})
   #print(day_str, ":", sum(len(ps) for ps in expeditions_day.values()), "participants", file=sys.stderr)
 
   # Try extending current streaks
   extended_streaks = []
-  for (start_date, groups) in streaks:
+  for (start_date, graticule, groups) in streaks:
     extended_groups = {}
     for ps, es in groups.items():
-      for new_e, new_ps in expeditions_day.items():
-        for new_p in new_ps:
-          if new_p not in ps:
-            # We can grow the group!
-            extended_ps = frozenset({*ps, new_p})
-            if extended_ps not in extended_groups:
-              # If we found a different way to build the same group, ignore this
-              extended_groups[extended_ps] = [*es, (new_e, new_p)]
+      for new_p in expeditions_day.get(graticule, []):
+        if new_p not in ps:
+          # We can grow the group!
+          extended_ps = frozenset({*ps, new_p})
+          if extended_ps not in extended_groups:
+            # If we found a different way to build the same group, ignore this
+            extended_groups[extended_ps] = [*es, new_p]
     if extended_groups:
       if len(extended_groups) > 100000:
         print("warning:", len(extended_groups), "possible groups from", start_date, "to", day_str)
-      extended_streaks.append((start_date, extended_groups))
+      extended_streaks.append((start_date, graticule, extended_groups))
     else:
       # Current streaks were maximal, report one
       example_group = next(iter(groups.values()))
-      if len(example_group) >= 7:
-        print(start_date, len(groups), len(example_group), example_group)
+      if len(example_group) >= 4:
+        print(start_date, graticule, len(groups), len(example_group), example_group)
 
   streaks = extended_streaks
 
   # Start a new streak
-  if expeditions_day:
-    streaks.append((day_str, {frozenset([p]): [(e, p)] for e, ps in expeditions_day.items() for p in ps}))
+  for g, ps in expeditions_day.items():
+    streaks.append((day_str, g, {frozenset([p]): [p] for p in ps}))
 
   # Advance
-  day -= timedelta(days=1)
+  day += timedelta(days=1)
 
