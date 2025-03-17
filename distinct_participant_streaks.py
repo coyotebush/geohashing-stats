a79e50509c947d347dac08039b4c1a8254ed2eb1
Corey Ford <corey@coreyford.name>
2025-03-17 15:10:05 +0100

    CPU time limit per day

--- distinct_participant_streaks.py
+++ distinct_participant_streaks.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import time
 import sys
 from datetime import date, timedelta
 
@@ -16,6 +17,8 @@ parser.add_argument('--graticule', action='append',
                          'Example: --graticule=-37_144 --graticule=-37_145')
 parser.add_argument('--same-graticule', action='store_true',
                     help='Only consider streaks within the same graticule.')
+parser.add_argument('--timeout', type=float, default='inf',
+                    help='Maximum time, in seconds, to spend on each date.')
 parser.add_argument('-v', '--verbose', action='count', default=0)
 args = parser.parse_args()
 
@@ -50,31 +53,37 @@ print("Loaded expeditions for", len(all_expeditions),
       "graticules" if args.same_graticule else "dates",
       file=sys.stderr)
 
-def find_longest_streak(expeditions, day, participants_seen, sequence):
+def find_longest_streak(expeditions, start_time, day, participants_seen, sequence):
   """
-  Returns the longest possible streak within the expeditions, starting on the given day,
-  not using any of the participants_seen, and appended to a previous sequence.
+  Returns the longest possible streak within the expeditions that can be found within the timeout,
+  starting on the given day, not using any of the participants_seen, and appended to a previous sequence.
   Also returns the set of participants whose presence in participants_seen (or some extended
   version thereof) directly or indirectly led to running out of ways to continue a streak.
+  Finally, returns whether the timeout was reached before potentially finding a longer streak.
   """
   if args.verbose >= 2:
-    print("Visiting", day, end='\r', file=sys.stderr)
+    print("Visiting", day, "without", "".join(p[0] for (p, _e) in sequence), end='\x1b[0K\r', file=sys.stderr)
   expeditions_day = expeditions.get(day, {})
   participants_day = set(expeditions_day.keys())
   participants_available = participants_day.difference(participants_seen)
   participants_unavailable = participants_day.intersection(participants_seen)
 
+  if time.process_time() - start_time > args.timeout:
+    # Timed out
+    return sequence, participants_unavailable, True
+
   if not participants_available:
     # Report who would need to be available to continue the streak
-    return sequence, participants_day
+    return sequence, participants_day, False
 
   limiting_participants = set()
 
   longest_sequence = sequence
 
   for participant in participants_available:
-    complete_sequence, next_participants \
+    complete_sequence, next_participants, timed_out \
         = find_longest_streak(expeditions,
+                              start_time,
                               day + step,
                               participants_seen.union([participant]),
                               [*sequence, (participant, expeditions_day[participant])])
@@ -82,6 +91,8 @@ def find_longest_streak(expeditions, day, participants_seen, sequence):
       longest_sequence = complete_sequence
 
     limiting_participants.update(next_participants)
+    if timed_out:
+      return longest_sequence, limiting_participants, True
     if participant in next_participants:
       # Choices that were unavailable to us might also be relevant
       limiting_participants.update(participants_unavailable)
@@ -93,7 +104,7 @@ def find_longest_streak(expeditions, day, participants_seen, sequence):
     else:
       # No use continuing this loop!
       break
-  return longest_sequence, limiting_participants
+  return longest_sequence, limiting_participants, False
 
 def upper_bound_streaks(expeditions, day):
   participants_count_day = len(expeditions.get(day, {}))
@@ -106,7 +117,7 @@ def print_longest_streak(seen_end_dates, expeditions, start_date):
   if args.verbose >= 1:
     print("Starting from", start_date, file=sys.stderr)
     print("Estimated possible sequences:", upper_bound_streaks(expeditions, start_date), file=sys.stderr)
-  some_longest_sequence, limiting_participants = find_longest_streak(expeditions, start_date, set(), [])
+  some_longest_sequence, limiting_participants, timed_out = find_longest_streak(expeditions, time.process_time(), start_date, set(), [])
   if args.verbose >= 1:
     print("Limited by", limiting_participants, file=sys.stderr) # To make sure there are some non-trivial cases
 
@@ -116,7 +127,7 @@ def print_longest_streak(seen_end_dates, expeditions, start_date):
     seen_end_dates.add(end_date)
     print("|-")
     print("|", start_date)
-    print("|", longest_length)
+    print("|", longest_length, "+" if timed_out else "")
     print("|", end="")
     for participant, expedition in some_longest_sequence:
       print(" [[", expedition, "|", participant, "]],", sep="", end="")
